0621e22a17327a2a584e4638c0e2fdb54fc459c0
vagrant <vagrant@vagrant.vm>
2021-03-29 22:47:02 +0000

    initilized

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk   # create the GUI 
+from tkinter import filedialog, Text # select the app text helps us display text
+import os # helps us run the apps we will import 
+
+
+root =tk.Tk()
+
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#262D42")
+canvas.pack()
+root.mainloop()
